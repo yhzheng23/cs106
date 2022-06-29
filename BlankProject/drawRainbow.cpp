7e770a946366d57c7bbab61a5fc222f724947120
Yuhua <jennyyuhua@gmail.com>
2022-06-29 11:15:02 +0800

    cs106b draw rainbow

---
+++ BlankProject/drawRainbow.cpp
@@ -0,0 +1,48 @@
+/*
+ * File: main.cpp
+ * --------------
+ * Blank C++ project configured to use Stanford cslib and Qt
+ */
+
+#include <cmath>
+#include "console.h"
+#include "simpio.h"
+using namespace std;
+
+#include "gwindow.h"
+
+int main()
+{
+    //int n = getInteger("Enter the number of darts : ");
+    //cout << "there are " << n << " darts." << endl;
+
+    //draw canvas
+    GWindow gw;
+    gw.setBackground("CYAN");
+    //draw circles: red, orange, yellow, green, blue, and magenta
+    double w = gw.getWidth();
+    double h = gw.getHeight();
+
+    double band =20;
+    double rmin = w/sqrt(2);
+    double xoffset = -(rmin-w/2);
+    double yoffset = (1-1/sqrt(2))*rmin;
+
+    gw.setColor("RED");
+    gw.fillOval(xoffset-band*6,h-(yoffset+band*6),(rmin+band*6)*2,(rmin+band*6)*2);
+    gw.setColor("ORANGE");
+    gw.fillOval(xoffset-band*5,h-(yoffset+band*5),(rmin+band*5)*2,(rmin+band*5)*2);
+    gw.setColor("YELLOW");
+    gw.fillOval(xoffset-band*4,h-(yoffset+band*4),(rmin+band*4)*2,(rmin+band*4)*2);
+    gw.setColor("GREEN");
+    gw.fillOval(xoffset-band*3,h-(yoffset+band*3),(rmin+band*3)*2,(rmin+band*3)*2);
+    gw.setColor("BLUE");
+    gw.fillOval(xoffset-band*2,h-(yoffset+band*2),(rmin+band*2)*2,(rmin+band*2)*2);
+    gw.setColor("MAGENTA");
+    gw.fillOval(xoffset-band*1,h-(yoffset+band),(rmin+band)*2,(rmin+band)*2);
+    gw.setColor("CYAN");
+    gw.fillOval(xoffset,h-yoffset,rmin*2,rmin*2);
+
+    return 0;
+}
+
